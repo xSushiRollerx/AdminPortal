fb962a0bc6a7f5b63cd2194cbd1e9eb903ec4c8a
tsemayess <tsemaye.oseragbaje@smoothstack.com>
2021-07-01 12:05:15 -0400

    Added navigation bar from customerportal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import './App.css';
+import HeaderComponent from './components/Navigation/HeaderComponent';
+import { makeStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import RestaurantSearch from './pages/RestaurantSearch'
+import clsx from 'clsx';
+import './App.css';
+import RestaurantSearch from './pages/RestaurantSearch';
+
+const useStyles = makeStyles((theme) => ({
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: 100,
+        marginRight: 100,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+}));
 
 function App() {
+    const classes = useStyles();
     return (
         <div>
             
             <Router>
-                    <div className="container">
+                <div className="container">
+                    <div className={clsx(classes.content)}>
+                        <div className={classes.drawerHeader} />
+                        <HeaderComponent />
                         <Switch>
                         <Route path="/restaurants" exact component={RestaurantSearch}></Route>
                         </Switch>
+                        </div>
                     </div>
             </Router>
         </div>
