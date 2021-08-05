eff56d04a8a9787325ece4c8c037310dc3f50959
tsemayess <tsemaye.oseragbaje@smoothstack.com>
2021-08-05 16:28:31 -0400

    Removed unused ButtonBase import

--- src/pages/RestaurantSearch.jsx
+++ src/pages/RestaurantSearch.jsx
@@ -3,7 +3,6 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Divider from '@material-ui/core/Divider';
 import TextField from '@material-ui/core/TextField';
-import ButtonBase from '@material-ui/core/ButtonBase';
 import SearchFilter from '../components/RestaurantSearch/SearchFilter';
 import RestaurantTable from '../components/RestaurantSearch/RestaurantTable';
 import TablePagination from '@material-ui/core/TablePagination';
